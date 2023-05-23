f810333ff73abb43b62daffc5ea6b50f1d5298a3
carlossmsqt <carlossmessquita@gmail.com>
2023-05-22 21:52:29 -0300

    Finalizando UsuarioDao, LoginBean e index.xhtml

--- ap2-dac/src/main/java/bean/LoginBean.java
+++ ap2-dac/src/main/java/bean/LoginBean.java
@@ -2,9 +2,27 @@ package bean;
 
 import javax.faces.bean.ManagedBean;
 
+import dao.UsuarioDao;
+import entidades.Usuario;
+import static util.MessageUtil.erro;
+
 @ManagedBean
 public class LoginBean {
+	private Usuario usuario = new Usuario();
+
 	public String logar() {
-		return "listagem_incidente.xhtml?faces-redirect=true";
+		Boolean usuarioEncontrado = verificarCadastro(usuario.getLogin(), usuario.getSenha());
+		
+		if (usuarioEncontrado) {
+			return "listagem_incidente.xhtml?faces-redirect=true";
+		} else {
+			erro("Erro", "Usuario não encontrado na base de dados. Faça seu cadastro!");
+			return null;
+		}
+	}
+	public Boolean verificarCadastro(String login, String senha) {
+		Boolean usuarioEncontrado = UsuarioDao.encontraPorLoginESenha(login, senha);
+		return usuarioEncontrado;
+		
 	}
 }
--- ap2-dac/src/main/java/dao/UsuarioDao.java
+++ ap2-dac/src/main/java/dao/UsuarioDao.java
@@ -45,6 +45,25 @@ public class UsuarioDao {
 		return lista;
 	}
 	
+	public static Boolean encontraPorLoginESenha(String login, String senha) {
+		EntityManager em = JPAUtil.criarEntityManager();
+		String jpql = "select u from Usuario u where u.login = :login and u.senha = :senha";
+		
+		try {
+			em.createQuery(jpql, Usuario.class).
+					setParameter("login", login).
+					setParameter("senha", senha).getResultList();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+			return false;
+		} finally {
+			em.close();
+		}
+	}
+	
 	// implementar login() ???
+	// sim!!!
 		
 }
